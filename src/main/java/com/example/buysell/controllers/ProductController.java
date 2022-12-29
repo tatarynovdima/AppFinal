707062f396eb00eb1cadcd7c0a0fe3287d79a460
dimat <dmytro.course@gmail.com>
2022-12-29 10:28:48 +0100

    FinalApp

---
+++ src/main/java/com/example/buysell/controllers/ProductController.java
@@ -0,0 +1,103 @@
+package com.example.buysell.controllers;
+
+import com.example.buysell.models.Product;
+import com.example.buysell.models.User;
+import com.example.buysell.services.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.security.Principal;
+
+
+/** Класс описывающий контроллер класса Product
+ * @author Dmytro Tatarynov <dmytro.course@gmail.com>
+ * @version 1.0
+ * @see ProductService
+ * */
+
+@Controller
+@RequiredArgsConstructor
+public class ProductController {
+
+
+    /** Внедрение зависимости IO, с помощью инверсии управления спринга добавляем сервис
+     * @RequiredArgsConstructor чтобы не добавлять конструктор используем анотацию
+     * @see ProductService помечен анотацией @Service то он является компонентом,
+     * таким образом контейнер спринга, его контекст будет создавать данный бин
+     */
+
+    private final ProductService productService;
+
+
+    @GetMapping("/")
+    public String products(@RequestParam(name = "searchWord", required = false) String title, Principal principal, Model model) {
+        model.addAttribute("products", productService.listProducts(title));
+        model.addAttribute("user", productService.getUserByPrincipal(principal));
+        model.addAttribute("searchWord", title);
+        return "products";
+    }
+
+
+    /**
+     * Метод получения информации о продукте
+     * @param id индификатор
+     * @param model
+     * @param principal
+     * @return Присвоения ссылки
+     */
+
+    @GetMapping("/product/{id}")
+    public String productInfo(@PathVariable Long id, Model model, Principal principal) {
+        Product product = productService.getProductById(id);
+        model.addAttribute("user", productService.getUserByPrincipal(principal));
+        model.addAttribute("product", product);
+        model.addAttribute("images", product.getImages());
+        model.addAttribute("authorProduct", product.getUser());
+        return "product-info";
+    }
+
+
+    /**
+     * Метод создание нового товара
+     * @param file1
+     * @param file2
+     * @param file3
+     * @param product товар
+     * @return сохранит новый товар и обновит странницу
+     * @throws IOException
+     */
+
+    @PostMapping("/product/create")
+    public String createProduct(@RequestParam("file1") MultipartFile file1, @RequestParam("file2") MultipartFile file2,
+                                @RequestParam("file3") MultipartFile file3, Product product, Principal principal) throws IOException {
+        productService.saveProduct(principal, product, file1, file2, file3);
+        return "redirect:/my/products";
+    }
+
+
+    /**
+     * Метод удаление товара
+     * @param id индификатор
+     * @return удалит товар и обновит странницу
+     */
+    @PostMapping("/product/delete/{id}")
+    public String deleteProduct(@PathVariable Long id, Principal principal) {
+        productService.deleteProduct(productService.getUserByPrincipal(principal), id);
+        return "redirect:/my/products";
+    }
+
+    @GetMapping("/my/products")
+    public String userProducts(Principal principal, Model model) {
+        User user = productService.getUserByPrincipal(principal);
+        model.addAttribute("user", user);
+        model.addAttribute("products", user.getProducts());
+        return "my-products";
+    }
+}
